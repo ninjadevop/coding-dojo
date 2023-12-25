535b040746c473c3f4dae3520456d54327038f06
ninjadevop <oussemahelali3@gmail.com>
2023-12-25 17:06:19 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/first_react/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo !</h1>
+      <h2>Things i need to do :</h2>
+      <ul>
+      <ul>Learn React</ul>
+      <ul>Climb Mt. Everset</ul>
+      <ul>Run a Marathon </ul>
+      <ul>Feed the Dogs</ul>
+      </ul>
+    </div>
+  );
+}
+export default App;
